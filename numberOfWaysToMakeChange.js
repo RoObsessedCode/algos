7d96f1d61049010d88a6bbabcbc7393f08bd1008
Rohan Saigal <rohan4saigal@gmail.com>
2018-02-09 09:04:49 -0500

    edited file for slight optimizations

--- numberOfWaysToMakeChange.js
+++ numberOfWaysToMakeChange.js
@@ -1,19 +1,20 @@
 function numberOfWaysToMakeChange(n, denoms) {
-
+  //fill array with n + 1 element and fill all with 0
+  //n + 1 because we need to account for amount===0
   const ways = (new Array(n + 1)).fill(0)
+  //how many ways are there to get nothing....1 way
   ways[0] = 1
 
-  for (let den = 0; den < denoms.length; den++) {
-    for (let amount = 1; amount < n + 1; amount++) {
-      if (denoms[den] <= amount) {
-        ways[amount] += ways[amount - denoms[den]]
+  //logic for each denom
+  for (let i = 0; i < denoms.length; i++) {
+    let denom = denoms[i]
+    for (let amount = 1; amount < (n + 1); amount++) {
+      //equals to because (denom - amount === 0) then ways[0]
+      // is an actual index in the array we need to account for
+      if (denom <= amount) {
+        ways[amount] += ways[amount - denom]
       }
     }
   }
   return ways[n]
-
 }
-
-const num = numberOfWaysToMakeChange(10, [1, 5])
-
-console.log(num)
