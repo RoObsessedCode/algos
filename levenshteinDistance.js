874b169cb9d5f4ad16abcd0df893750b7023ca18
Rohan Saigal <rohan4saigal@gmail.com>
2018-02-13 21:47:44 -0500

    second completion w/o plugging into algoexperts

--- levenshteinDistance.js
+++ levenshteinDistance.js
@@ -1,27 +1,27 @@
 function levenshteinDistance(str1, str2) {
-  // Write your code here.
   const edits = []
-  for (let i = 0; i < str2.length + 1; i++) {
+  for (let i = 0; i < str1.length + 1; i++) {
     const row = []
-    for (let j = 0; j < str1.length + 1; j++) {
+    for (let j = 0; j < str2.length + 1; j++) {
       row.push(j);
     }
     row[0] = i;
     edits.push(row)
-  }//^^^ Create 2-D Matrix ^^^
+  }
 
-
-  for (let i = 1; i < str2.length + 1; i++) {
-    for (let j = 1; j < str1.length + 1; j++) {
-      if (str2[i - 1] === str1[j - 1]) {
-        edits[i][j] = edits[i - 1][j - 1]
+  for (let i = 1; i < str1.length + 1; i++) {
+    for (let j = 1; j < str2.length + 1; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        edits[i][j] = edits[i-1][j-1]
       } else {
         edits[i][j] = 1 + Math.min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1])
       }
     }
   }
-  return edits[str2.length][str1.length]
+
+  return edits[str1.length][str2.length]
+
 }
 
-console.log(levenshteinDistance('abc', 'yabd'))
+console.log(levenshteinDistance("abc", "yabd"));
 //insertion of, deletion of, and substitution of another character
