46c4e2a6da2d0c6fbcf5bd7f02330e0bad021896
Rohan Saigal <rohan4saigal@gmail.com>
2018-03-21 09:29:45 -0400

    merge sort

---
+++ mergeSort.js
@@ -0,0 +1,40 @@
+
+
+const mergeSort = function (array) {
+  if (array.length < 2) return array
+  const splits = split(array),
+    left = splits[0],
+    right = splits[1],
+  return merge(mergeSort(left), mergeSort(right))
+
+
+
+  const split = function (array) {
+    const center = array.length / 2
+          left = array.slice(0, center),
+          right = array.slice(center);
+    return [left, right]
+  }
+
+
+  const merge = function(left, right) {
+    const merged = []
+    let leftIdx = 0,
+        rightIdx = 0
+    while (leftIdx < left.length && rightIdx < right.length) {
+      if (left[leftIdx] < right[rightIdx]) {
+        merged.push(left[leftIdx++])
+      } else {
+        merged.push(right[rightIdx++])
+      }
+    }
+    for (; leftIdx < left.length; leftIdx++) merged.push(left[leftIdx]);
+    for (; rightIdx < right.length; rightIdx++) merged.push(right[rightIdx])
+
+    return merged;
+
+
+  }
+
+
+}
